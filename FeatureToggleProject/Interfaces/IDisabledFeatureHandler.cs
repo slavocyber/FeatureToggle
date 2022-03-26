3dc7678efd926f9a01d7600fbb017d1429c3ba5f
ViacheslavZhurbenko <milkscrew@gmail.com>
2022-03-26 21:00:40 +0200

    Add CustomDisableFeatureHandler and interface

---
+++ FeatureToggleProject/Interfaces/IDisabledFeatureHandler.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FeaturesMaster.Interfaces
+{
+    public interface IDisabledFeatureHandler
+    {
+        Task HandleDisabledFeatures(IEnumerable<string> features, ActionExecutingContext context);
+    }
+}
