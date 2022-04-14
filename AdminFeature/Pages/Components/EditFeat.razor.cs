63d4a80a02d9b2651054afbb7960149dad1633c0
ViacheslavZhurbenko <milkscrew@gmail.com>
2022-04-14 22:29:04 +0300

    Update EditFeat

---
+++ AdminFeature/Pages/Components/EditFeat.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AdminFeature.Pages.Components;
+
+public partial class EditFeat
+{
+    [Parameter]
+    public EventCallback Delete { get; set; }
+
+    [Parameter]
+    public EventCallback<string> Add { get; set; }
+
+    private bool _invalidFeatName;
+    private string? _newName;
+    private bool _editButtonStatus;
+
+    private void EditButtonStatus()
+    {
+        _editButtonStatus = !_editButtonStatus;
+    }
+
+    private async Task Edit()
+    {
+        try
+        {
+            await Add.InvokeAsync(_newName);
+
+            _ = Delete.InvokeAsync();
+            EditButtonStatus();
+        }
+        catch (Exception)
+        {
+
+            _invalidFeatName = true;
+        }
+    }
+}
